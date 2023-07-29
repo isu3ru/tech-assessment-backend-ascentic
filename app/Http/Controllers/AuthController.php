6c887c53e163cf23f19ab506ac5c6f7204d5f40b
Isuru Ranawaka <isu3ru@gmail.com>
2023-07-29 12:30:52 +0530

    API Resources User and IbanValidation API resources Collection for Iban validation pagination result

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -9,47 +9,42 @@ use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
 use Illuminate\Validation\ValidationException;
 
-class AuthController extends Controller
-{
-    public function __construct(public AuthService $authService)
-    {
-    }
+class AuthController extends Controller {
+	public function __construct(public AuthService $authService) {
+	}
 
-    /**
-     * Process user sign in request and issue token
-     */
-    function signIn(UserSignInRequest $request): JsonResponse|ValidationException
-    {
-        $token = $this->authService->signIn($request->validated());
+	/**
+	 * Process user sign in request and issue token
+	 */
+	function signIn(UserSignInRequest $request): JsonResponse | ValidationException {
+		$token = $this->authService->signIn($request->validated());
 
-        if (!$token && is_bool($token)) {
-            return response()->json(['message' => 'Invalid credentials'], 401);
-        }
+		if (!$token && is_bool($token)) {
+			return response()->json(['message' => 'Invalid credentials'], 401);
+		}
 
-        return response()->json(['token' => $token]);
-    }
+		return response()->json(['token' => $token]);
+	}
 
-    /**
-     * Process user sign up request
-     */
-    function signUp(UserSignUpRequest $request): JsonResponse|ValidationException
-    {
-        $user = $this->authService->signUp($request->validated());
+	/**
+	 * Process user sign up request
+	 */
+	function signUp(UserSignUpRequest $request): JsonResponse | ValidationException {
+		[$user, $token] = $this->authService->signUp($request->validated());
 
-        if (!$user) {
-            return response()->json(['message' => 'Failed to sign up user'], 500);
-        }
+		if (!$user) {
+			return response()->json(['message' => 'Failed to sign up user'], 500);
+		}
 
-        return response()->json(['user' => $user], 201);
-    }
+		return response()->json(compact('user', 'token'), 201);
+	}
 
-    /**
-     * Process user sign out request
-     */
-    function signOut(Request $request): JsonResponse
-    {
-        $this->authService->signOut($request->user());
+	/**
+	 * Process user sign out request
+	 */
+	function signOut(Request $request): JsonResponse {
+		$this->authService->signOut($request->user());
 
-        return response()->json(['message' => 'User signed out successfully']);
-    }
+		return response()->json(['message' => 'User signed out successfully']);
+	}
 }
